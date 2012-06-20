5b58a763a1ae432a5382a4dcc4f5d0a542de37ec
Pablo Martí <pablo.marti89@gmail.com>
2012-06-19 21:42:58 -0600

    Slider se paso a horizontal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,3 +56,32 @@ addEvent(window, 'popstate', function (event) {
     reportEvent(event.state || { url: "unknown", name: "undefined" });
   }
 });
+
+jQuery.fn.extend({
+  slideRightShow: function() {
+    return this.each(function() {
+        $(this).show('slide', {direction: 'right'}, 700);
+    });
+  },
+  slideLeftHide: function(element) {
+    return this.each(function() {
+      $(this).hide('slide', {
+          direction: 'left',
+          complete:function(){
+            $(this).remove();
+            element.slideRightShow();
+          }
+        }, 700);      
+    });
+  },
+  slideRightHide: function() {
+    return this.each(function() {
+      $(this).hide('slide', {direction: 'right'}, 1000);
+    });
+  },
+  slideLeftShow: function() {
+    return this.each(function() {
+      $(this).show('slide', {direction: 'left'}, 1000);
+    });
+  }
+});
